d5490f7cef3c3d8f558b7f8e3597c2e133b90e1c
Sergei Oskolkov <SergeyOsk2017@gmail.com>
2024-08-21 01:03:24 +0200

    Added profile, deleted, and rating fields to Author model

--- library/models.py
+++ library/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
+    profile = models.URLField(null=True, blank=True)  # Профиль автора
+    deleted = models.BooleanField(default=False)  # Поле для пометки автора как удаленного
+    rating = models.IntegerField(
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
